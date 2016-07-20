568688a74dddcc18b29e1b1c3ed6d6e922b2ae99
Noah Wimmer <nwimmer123@yahoo.com>
2016-07-20 13:49:02 -0700

    brough in index

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var app = express();
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
-app.use('/', express.static(__dirname + '/public'));
+app.use('/',express.static(__dirname + '/public'));
 
 // ROUTES
 
-app.get('/', function (req,res) {
-  res.sendFile(__dirname + '/pages/index.html');
+app.get('/', function(req,res) {
+  res.render('pages/index');
 });
 
 //SERVER
